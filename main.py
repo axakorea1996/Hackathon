6d2ef1e9baade396cbab31f8b7a13a4136671fac
axakorea1996 <axakorea1996@gmail.com>
2026-04-10 10:12:07 +0900

    testsdfsdf

--- main.py
+++ main.py
@@ -146,9 +146,9 @@ async def add_security_headers(request: Request, call_next):
     response.headers["Strict-Transport-Security"] = "max-age=31536000; includeSubDomains"
     response.headers["Server"] = "webserver"
     response.headers["Content-Security-Policy"] = (
-        f"default-src 'self'; "
-        f"style-src 'self' 'unsafe-inline'; "
-        f"script-src 'self' 'nonce-{nonce}'"
+     "default-src 'self'; "
+     "style-src 'self' 'unsafe-inline'; "
+     "script-src 'self' 'unsafe-inline'"
     )
     return response
 
